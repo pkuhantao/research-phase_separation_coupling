f664e8f92369d1a03760ea410b51b81056ecc75f
pkuhantao <pkuhantao@gmail.com>
2014-07-06 12:20:31 -0400

    minor modifies on NormalGrids2D: add comments and 0.000001 in floor function...

--- NormalGrids2D.cpp
+++ NormalGrids2D.cpp
@@ -9,15 +9,15 @@
 #include "NormalGrids2D.h"
 #include <cassert>
 #include <cmath>
-#include <iostream>
 
 // constructor
 NormalGrids2D::NormalGrids2D(int NTheta, int NPhi, double r) {
     // step
     dTheta = (thetaMax-thetaMin) / NTheta;
     dPhi = (phiMax-phiMin) / NPhi;
+    // radius
     radius = r;
-    // vector
+    // theta and phi value
     thetaVal.resize(NTheta+1, 0);
     phiVal.resize(NPhi+1, 0);
     for (int i = 0; i <= NTheta; i++) thetaVal[i] = thetaMin + i*dTheta;
@@ -33,6 +33,7 @@ NormalGrids2D::NormalGrids2D(int NTheta, int NPhi, double r) {
     x1.resize(nrol, vector<double>(ncol, 0));
     y1.resize(nrol, vector<double>(ncol, 0));
     z1.resize(nrol, vector<double>(ncol, 0));
+    // calculate (x, y, z) in its own coordinates
     for (int i = 0; i < nrol; i++) {
         for (int j = 0; j < ncol; j++) {
             x0[i][j] = radius * sin(thetaVal[i]) * cos(phiVal[j]);
@@ -40,6 +41,7 @@ NormalGrids2D::NormalGrids2D(int NTheta, int NPhi, double r) {
             z0[i][j] = radius * cos(thetaVal[i]);
         }
     }
+    // calculate (x, y, z) in its opposite coordinates
     for (int i = 0; i < nrol; i++) {
         for (int j = 0; j < ncol; j++) {
             x1[i][j] = -x0[i][j];
@@ -49,58 +51,71 @@ NormalGrids2D::NormalGrids2D(int NTheta, int NPhi, double r) {
     }
 }
 
+// theta
 double NormalGrids2D::theta(int i) const {
     assert(i >= 0 && i < (int)thetaVal.size());
     return thetaVal[i];
 }
 
-
-double NormalGrids2D::phi(int i) const {
-    assert(i >= 0 && i < (int)phiVal.size());
-    return phiVal[i];
+// phi
+double NormalGrids2D::phi(int j) const {
+    assert(j >= 0 && j < (int)phiVal.size());
+    return phiVal[j];
 }
 
+// x
 double NormalGrids2D::x(int i, int j) const {
     assert(i >= 0 && i < (int)thetaVal.size());
     assert(j >= 0 && j < (int)phiVal.size());
     return x0[i][j];
 }
 
+// y
 double NormalGrids2D::y(int i, int j) const {
     assert(i >= 0 && i < (int)thetaVal.size());
     assert(j >= 0 && j < (int)phiVal.size());
     return y0[i][j];
 }
 
+// z
 double NormalGrids2D::z(int i, int j) const {
     assert(i >= 0 && i < (int)thetaVal.size());
     assert(j >= 0 && j < (int)phiVal.size());
     return z0[i][j];
 }
 
+// x'
 double NormalGrids2D::xprime(int i, int j) const {
     assert(i >= 0 && i < (int)thetaVal.size());
     assert(j >= 0 && j < (int)phiVal.size());
     return x1[i][j];
 }
 
+// y'
 double NormalGrids2D::yprime(int i, int j) const {
     assert(i >= 0 && i < (int)thetaVal.size());
     assert(j >= 0 && j < (int)phiVal.size());
     return y1[i][j];
 }
 
+// z'
 double NormalGrids2D::zprime(int i, int j) const {
     assert(i >= 0 && i < (int)thetaVal.size());
     assert(j >= 0 && j < (int)phiVal.size());
     return z1[i][j];
 }
 
-
+// given theta angle, return the index for that angle(round downwards)
 int NormalGrids2D::thetaToIndex(double angle) const {
-    return (int)floor((angle-thetaMin)/dTheta);
+    return (int)floor((angle-thetaMin)/dTheta + 0.000001);
 }
 
+// given phi angle, return the index for that angle(round downwards)
 int NormalGrids2D::phiToIndex(double angle) const {
-    return (int)floor((angle-phiMin)/dPhi);
+    return (int)floor((angle-phiMin)/dPhi + 0.000001);
 }
+
+
+
+
+
--- NormalGrids2D.h
+++ NormalGrids2D.h
@@ -9,7 +9,6 @@
 #ifndef ____NormalGrids2D__
 #define ____NormalGrids2D__
 
-#include <iostream>
 #include <vector>
 
 #define PI 3.14159265
@@ -19,20 +18,24 @@ using namespace std;
 
 class NormalGrids2D {
 public:
-    // pass in number of cells in theta and phi direction
+    // pass in number of cells in theta and phi direction, and the radius
     NormalGrids2D(int NTheta, int NPhi, double r);
     ~NormalGrids2D(){};
-    int sizeInTheta() const {return thetaVal.size();}; // number of points on theta axis
+    int sizeInTheta() const {return thetaVal.size();}; // number of points on theta axis != number of cells
     int sizeInPhi() const {return phiVal.size();}; // number of points on phi axis
-    double dTHETA() const {return dTheta;};
+    
+    double dTHETA() const {return dTheta;}; // steps
     double dPHI() const {return dPhi;};
-    double rad() const {return radius;};
-    double theta(int i) const;
-    double phi(int i) const;
-    double x(int i, int j) const;
+    
+    double rad() const {return radius;};  // radius
+    double theta(int i) const;  // theta
+    double phi(int j) const; // phi
+    
+    double x(int i, int j) const; // x, y, z in itw own coordinates
     double y(int i, int j) const;
     double z(int i, int j) const;
-    double xprime(int i, int j) const;
+    
+    double xprime(int i, int j) const; // (x, y, z) in its opposite coordinates
     double yprime(int i, int j) const;
     double zprime(int i, int j) const;
     
@@ -40,17 +43,21 @@ public:
     int phiToIndex(double angle) const; // given phi angle, return the index for that angle(round downwards)
     
 private:
-    static const double thetaMin = PI/4.0;
+    static const double thetaMin = PI/4.0;  // (theta, phi) range in Yin/Yang grids
     static const double thetaMax = 3*PI/4.0;
     static const double phiMin = -3*PI/4.0;
     static const double phiMax = 3*PI/4.0;
     
-    double dTheta;
+    double dTheta;  // steps
     double dPhi;
-    double radius;
-    vector<double> thetaVal;
+    
+    double radius;  // radius
+    
+    vector<double> thetaVal; // (theta, phi)
     vector<double> phiVal;
+    
     vector<vector<double> > x0, y0, z0; // (x, y, z) in its own coordinates
+    
     vector<vector<double> > x1, y1, z1; // (x, y, z) in its opposite coordinates
 };
 
