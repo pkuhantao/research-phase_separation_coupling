4b3b9d59dd3e54574dcc0925f83d852a53be0bae
pkuhantao <pkuhantao@gmail.com>
2014-07-06 16:38:34 -0400

    add comments in GhostBC2D

--- GhostBC2D.cpp
+++ GhostBC2D.cpp
@@ -14,26 +14,28 @@
 #include <utility>
 #include <vector>
 
+// constructor
 GhostBC2D::GhostBC2D(const vector<double> &theta, const vector<double> &phi, const NormalGrids2D &oppoGrids) {
     const int N = theta.size();
     angles.resize(N, pair<double, double>());
     anglesPrim.resize(N, pair<double, double>());
-    nbs.resize(N, vector<pair<int, int> >(numNbs, pair<int, int>())); // for each cell, 4 neighbors
+    nbs.resize(N, vector<pair<int, int> >(numNbs, pair<int, int>())); // for each cell, numNbs=4 neighbors
     weights.resize(N, vector<double>(numNbs, 0));
     // make a copy of angles
     for (int i = 0; i < N; i++) {
         angles[i].first = theta[i];
         angles[i].second = phi[i];
     }
-    // calculate corresponding angle in complemental coordinates
+    // calculate corresponding angle in complemental Yin/Yang coordinates
     coordiTrans();
-    // find all neighbors in complemental coordinates
+    // find all neighbors in complemental Yin/Yang coordinates
     findNbs(oppoGrids);
     // find all weights of each neighbor for interpolation
     calWts(oppoGrids);
 }
 
-// calcualte theta', phi' in complemental coordinate
+// from (theta, phi) in its own coordinates,
+// calcualte (theta', phi') in complemental Yin/Yang coordinate
 void GhostBC2D::coordiTrans() {
     for (int i = 0; i < angles.size(); i++) {
         double theta = angles[i].first;
@@ -50,7 +52,9 @@ void GhostBC2D::coordiTrans() {
     }
 }
 
-// from given complemental normal grids, find neighbors for interpolation
+// Given complemental Yin/Yang grids + anglesPrim, find neighbors for interpolation
+// here we assume the interpolation point has to fall into a cell in complemental
+// Yin/Yang grids
 void GhostBC2D::findNbs(const NormalGrids2D &grids) {
     for (int i = 0; i < anglesPrim.size(); i++) {
         double thetaPrim = anglesPrim[i].first;
@@ -58,6 +62,7 @@ void GhostBC2D::findNbs(const NormalGrids2D &grids) {
         
         int rowID = grids.thetaToIndex(thetaPrim);
         int colID = grids.phiToIndex(phiPrim);
+        // four neighbors' indice
         nbs[i][0] = pair<int, int>(rowID, colID);
         nbs[i][1] = pair<int, int>(rowID, colID+1);
         nbs[i][2] = pair<int, int>(rowID+1, colID);
@@ -65,7 +70,8 @@ void GhostBC2D::findNbs(const NormalGrids2D &grids) {
     }
 }
 
-// from the neighbors' position, calculate interpolation weights
+// from the neighbors' position in complemental Yin/Yang grids + anglesPrim,
+// calculate interpolation weights
 void GhostBC2D::calWts(const NormalGrids2D &grids) {
     for (int i = 0; i < weights.size(); i++) {
         double thetaPrim = anglesPrim[i].first;
@@ -76,6 +82,7 @@ void GhostBC2D::calWts(const NormalGrids2D &grids) {
         double phiPrim0 = grids.phi(nbs[i][0].second);
         double s0 = dThetaPrim * dPhiPrim;
         
+        // weights for four neighbors
         weights[i][0] = (thetaPrim0+dThetaPrim-thetaPrim) * (phiPrim0+dPhiPrim-phiPrim) / s0;
         weights[i][1] = (thetaPrim0+dThetaPrim-thetaPrim) * (phiPrim-phiPrim0) / s0;
         weights[i][2] = (thetaPrim-thetaPrim0) * (phiPrim0+dPhiPrim-phiPrim) / s0;
@@ -83,11 +90,13 @@ void GhostBC2D::calWts(const NormalGrids2D &grids) {
     }
 }
 
-// given f, interpolate f0
-void GhostBC2D::interpolation(vector<double> &f0, const vector<vector<double> > &f) {
+// given value f in opposite grids, interpolate f0 in this boundary in its own grids
+void GhostBC2D::interpolation(vector<double> &f0, const vector<vector<double> > &f) const {
     assert(f0.size() == weights.size());
+    // point i in this boundary
     for (int i = 0; i < f0.size(); i++) {
         f0[i] = 0;
+        // neighbor j for point i
         for (int j = 0; j < numNbs; j++) {
             f0[i] += weights[i][j] * f[nbs[i][j].first][nbs[i][j].second];
         }
--- GhostBC2D.h
+++ GhostBC2D.h
@@ -6,27 +6,29 @@
 //
 //
 
+// 2D ghost Boundary (given one boundary, create its interpolation scheme from the opposite Yin/Yang grids)
+
 #ifndef ____GhostBC2D__
 #define ____GhostBC2D__
 
 #include "NormalGrids2D.h"
 
-#include <iostream>
 #include <vector>
 #include <utility>
 
 
-#define PI 3.14159265
-
 using namespace std;
+
 class GhostBC2D {
 public:
+    // given the boundary definition(r=const, theta, phi) in its own grids and the opposite Yin/Yang grids,
+    // construct the interpolation scheme in this boundary
     GhostBC2D(const vector<double> &theta, const vector<double> &phi, const NormalGrids2D &oppoGrids);
     ~GhostBC2D(){};
-    void interpolation(vector<double> &f0, const vector<vector<double> > &f); // given f, interpolate f0
+    void interpolation(vector<double> &f0, const vector<vector<double> > &f) const; // given value f in opposite grids, interpolate f0 in this boundary in its own grids
 
 private:
-    void coordiTrans(); // calcualte theta', phi' in complemental coordinate
+    void coordiTrans(); // calcualte theta', phi' in opposite/complemental Yin/Yang grids
     void findNbs(const NormalGrids2D &grids); // from given complemental normal grids, find neighbors for interpolation
     void calWts(const NormalGrids2D &grids); // from the neighbors' position, calculate interpolation weights
     
