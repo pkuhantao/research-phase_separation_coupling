f6678e7f729fe5bb8fba62aa6b3b9e10df83d7c6
pkuhantao <pkuhantao@gmail.com>
2014-07-06 11:18:48 -0400

    add membrane properties and coupling strength into Properties.h

--- Properties.h
+++ Properties.h
@@ -10,15 +10,28 @@
 #define ____Properties__
 
 
+// inner solvent properties
 struct SolvProp {
-    double M;
-    double w;
+    double M; // mobility
+    double w; // coefficients in phase field energy
     double a;
     double b;
-    SolvProp() {M=0; w=0; a=0; b=0;};
-    SolvProp(double M1, double w1, double a1, double b1) : M(M1), w(w1), a(a1), b(b1) {};
+    double Lambda; // coupling strength
+    SolvProp() {M=1; w=1; a=1; b=1; Lambda=0;}; // by default, no coupling between inner solvent and membrane
+    SolvProp(double M1, double w1, double a1, double b1, double Lambda1) : M(M1), w(w1), a(a1), b(b1), Lambda(Lambda1) {};
 };
 
+// membrane properties
+struct MembProp {
+    double M; // mobility
+    double w; // coefficients in phase field energy
+    double a;
+    double b;
+    double Lambda; // coupling strength
+    double thickness; // thickness of the membrane
+    MembProp() {M=1; w=1; a=1; b=1; Lambda=0; thickness=1;}; // by default, no coupling between inner solvent and membrane
+    MembProp(double M1, double w1, double a1, double b1, double Lambda1, double thickness1) : M(M1), w(w1), a(a1), b(b1), Lambda(Lambda1), thickness(thickness1) {};
+};
 
 
 
