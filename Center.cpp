f277d6008b26600cb149125f641c647d70e3bfe9
pkuhantao <pkuhantao@gmail.com>
2014-07-06 21:40:59 -0400

    add interpolation part in Center

--- Center.cpp
+++ Center.cpp
@@ -20,16 +20,11 @@ using namespace std;
 
 Center::Center(double dt, SolvProp solvProp, const InnerSolvPatch &Yin, const InnerSolvPatch &Yang) {
     props = solvProp;
-    psi_Yin = &Yin.psi;
-    psi_Yang = &Yang.psi;
-    mu_Yin = &Yin.mu;
-    mu_Yang = &Yang.mu;
-    
     this->dt = dt;
     dr = Yin.grids3D->dRAD();
-    // in the following, we find the positions of center's neighbors in Yin and Yang grids
-    // there are three neighbors each in Yin and Yang grids, nbs_Yin[i][0..2] stands for index of (r, theta, phi)
-    // in Yin grids for neighbor i
+    // In the following, we find the positions of center's neighbors in Yin and Yang grids
+    // There are three neighbors each in Yin and Yang grids,
+    // nbs_Yin[i][0..2] stands for index of (r, theta, phi) in Yin grids for neighbor i
     nbs_Yin.resize(3, vector<int>(3, 0));
     nbs_Yang.resize(3, vector<int>(3, 0));
     // neighbors in Yin grids
@@ -52,23 +47,58 @@ Center::Center(double dt, SolvProp solvProp, const InnerSolvPatch &Yin, const In
         nbs_Yang[index][1] = thetaID;
         nbs_Yang[index][2] = phiID;
     }
+    // create vectors to store psi and mu in neighbor cells
+    psi_Yin_nbs.resize(3, 0);
+    psi_Yang_nbs.resize(3, 0);
+    mu_Yin_nbs.resize(3, 0);
+    mu_Yang_nbs.resize(3, 0);
+}
+
+// interpolate psi in neighboring cells from Yin and Yang grids
+void Center::interpPsi(const vector<vector<vector<double> > > &psi_Yin, const vector<vector<vector<double> > > &psi_Yang) {
+    // psi from Yin grids
+    for (int i = 0; i < nbs_Yin.size(); i++) {
+        int i1 = nbs_Yin[i][0];
+        int i2 = nbs_Yin[i][1];
+        int i3 = nbs_Yin[i][2];
+        psi_Yin_nbs[i] = psi_Yin[i1][i2][i3];
+    }
+    // psi from Yang grids
+    for (int i = 0; i < nbs_Yang.size(); i++) {
+        int i1 = nbs_Yang[i][0];
+        int i2 = nbs_Yang[i][1];
+        int i3 = nbs_Yang[i][2];
+        psi_Yang_nbs[i] = psi_Yang[i1][i2][i3];
+    }
+}
+
+// interpolate mu in neighboring cells from Yin and Yang grids
+void Center::interpMu(const vector<vector<vector<double> > > &mu_Yin, const vector<vector<vector<double> > > &mu_Yang) {
+    // mu from Yin grids
+    for (int i = 0; i < nbs_Yin.size(); i++) {
+        int i1 = nbs_Yin[i][0];
+        int i2 = nbs_Yin[i][1];
+        int i3 = nbs_Yin[i][2];
+        mu_Yin_nbs[i] = mu_Yin[i1][i2][i3];
+    }
+    // mu from Yang grids
+    for (int i = 0; i < nbs_Yang.size(); i++) {
+        int i1 = nbs_Yang[i][0];
+        int i2 = nbs_Yang[i][1];
+        int i3 = nbs_Yang[i][2];
+        mu_Yang_nbs[i] = mu_Yang[i1][i2][i3];
+    }
 }
 
 // calculate mu at the center from double well potential
 void Center::calcMu_dw() {
     // calcualte laplacian of psi at first
     double laplace = 0;
-    for (int i = 0; i < nbs_Yin.size(); i++) {
-        int i1 = nbs_Yin[i][0];
-        int i2 = nbs_Yin[i][1];
-        int i3 = nbs_Yin[i][2];
-        laplace += (*psi_Yin)[i1][i2][i3];
+    for (int i = 0; i < psi_Yin_nbs.size(); i++) {
+        laplace += psi_Yin_nbs[i];
     }
-    for (int i = 0; i < nbs_Yang.size(); i++) {
-        int i1 = nbs_Yang[i][0];
-        int i2 = nbs_Yang[i][1];
-        int i3 = nbs_Yang[i][2];
-        laplace += (*psi_Yang)[i1][i2][i3];
+    for (int i = 0; i < psi_Yang_nbs.size(); i++) {
+        laplace += psi_Yang_nbs[i];
     }
     laplace = (laplace-6.0*psi)/(dr*dr);
     
@@ -84,17 +114,11 @@ void Center::calcMu_sd() {
 void Center::updatePsi() {
     // calcualte laplacian of mu at first
     double laplace = 0;
-    for (int i = 0; i < nbs_Yin.size(); i++) {
-        int i1 = nbs_Yin[i][0];
-        int i2 = nbs_Yin[i][1];
-        int i3 = nbs_Yin[i][2];
-        laplace += (*mu_Yin)[i1][i2][i3];
+    for (int i = 0; i < mu_Yin_nbs.size(); i++) {
+        laplace += mu_Yin_nbs[i];
     }
-    for (int i = 0; i < nbs_Yang.size(); i++) {
-        int i1 = nbs_Yang[i][0];
-        int i2 = nbs_Yang[i][1];
-        int i3 = nbs_Yang[i][2];
-        laplace += (*mu_Yang)[i1][i2][i3];
+    for (int i = 0; i < mu_Yang_nbs.size(); i++) {
+        laplace += mu_Yang_nbs[i];
     }
     laplace = (laplace-6.0*mu)/(dr*dr);
     
--- Center.h
+++ Center.h
@@ -22,6 +22,9 @@ public:
     Center(double dt, SolvProp solvProp, const InnerSolvPatch &Yin, const InnerSolvPatch &Yang);
     ~Center() {};
     
+    void interpPsi(const vector<vector<vector<double> > > &psi_Yin, const vector<vector<vector<double> > > &psi_Yang); // interpolate psi in neighboring cells from Yin and Yang grids
+    void interpMu(const vector<vector<vector<double> > > &mu_Yin, const vector<vector<vector<double> > > &mu_Yang); // interpolate mu in neighboring cells from Yin and Yang grids
+    
     void calcMu_dw(); // calculate mu at the center from double well potential
     void calcMu_sd(); // calculate mu at the center from simple diffusion model, where mu = psi
     void updatePsi(); // update psi at the center from mu
@@ -32,15 +35,12 @@ public:
     
 private:
     SolvProp props; // inner solvent properties
-    const vector<vector<vector<double> > > *psi_Yin, *psi_Yang;
-    const vector<vector<vector<double> > > *mu_Yin, *mu_Yang;
-    
-    
     double dt; // time step
     double dr; // step in radius direction
     double psi; // psi at the center
     double mu; // mu at the center
     vector<vector<int> > nbs_Yin, nbs_Yang; // positions of neighbors of center in Yin-Yang grids, here we use the first order interpolation, assuming all neighbors fall exactly on the cells in Yin-Yang grids
+    vector<double> psi_Yin_nbs, psi_Yang_nbs, mu_Yin_nbs, mu_Yang_nbs; // storing psi and mu in neighboring cells
 };
 
 
