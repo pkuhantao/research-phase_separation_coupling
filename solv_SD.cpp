ec19b4d223cd99e6954d49f7420279d338a3547e
pkuhantao <pkuhantao@gmail.com>
2014-07-09 16:02:17 -0400

    modify solv_SD to work with input file

--- solv_SD.cpp
+++ solv_SD.cpp
@@ -6,52 +6,110 @@
 //
 //
 
+// simple diffusion in 3D inner solvent only
+
+#include "Properties.h"
 #include "InnerSolv.h"
 #include "util_funcs.h"
 
-#include <time.h>
-#include <stdlib.h>
 
-#include <string>
 #include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <tr1/unordered_map>
+#include <string>
+#include <sstream>
+#include <utility>
+#include <ctime>
 
 using namespace std;
 
+
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        cerr << "Wrong input: program + dt + Nstep!" << endl;
-        exit(1);
-    }
+    if (argc != 2) {
+		cerr << "Wrong inputs! Should be (./$program $input_filename)" << endl;
+		exit(1);
+	}
     
-    const double dt = atof(argv[1]);
-    const int Nstep = atoi(argv[2]);
-    const int NprintState = Nstep/20;
-    const int NprintAnaly = Nstep/20;
+    ifstream inputfile(argv[1]); // input file
+	tr1::unordered_map<string, string> paras; // parameters' map
     
-    const string datafolder = "./SD_data";
+    // open the file
+	if (inputfile.is_open()) {
+		// obtain all parameters and their value, and store into the map
+		while (inputfile.good()) {
+			string oneline;  // one pair of parameter and its value
+			getline(inputfile, oneline); // get one line from the file
+			if (oneline.empty()) continue;
+			istringstream iss(oneline);
+			string key, val;
+			iss >> key >> val;
+			paras.insert(make_pair<string, string>(key, val)); // add new pair into map
+		}
+		inputfile.close(); // close the parameters file
+	}
+	else {
+		cerr << "Unable to open file" << endl;
+		exit(1);
+	}
     
-    srand(time(NULL)); // initialize random seed
+    //parse parameters value
+	const string datafolder = paras["datafolder"]; // folder name for storing data
+	const int nthreads = atoi(paras["nthreads"].c_str()); // number of openmp threads
+    // space and time info
+    const double radius = atof(paras["radius"].c_str()); // radius
+    const int nR = atoi(paras["nR"].c_str()); // space dimensions
+    const int nTheta = atoi(paras["nTheta"].c_str());
+    const int nPhi = atoi(paras["nPhi"].c_str());
+    const int nt = atoi(paras["nt"].c_str()); // number of time steps
+    const int t_start = atoi(paras["t_start"].c_str()); // start time
+    const double dt = atof(paras["dt"].c_str()); // time step
+    // parameters for data storage and analysis
+	const int numConfig = atoi(paras["numConfig"].c_str()); // number of configurations
+	const int numAnaly = atoi(paras["numAnaly"].c_str());  // nmber of analysis
+	const int nPrintState = (nt - 1)/numConfig; // every nPrintState steps, store the state
+	const int nPrintAnaly = (nt - 1)/numAnaly; // every nPrintAnaly steps, store the analysis
+    // parameters for inner solvent
+    SolvProp solv_Props;
+    solv_Props.M = atof(paras["M_s"].c_str()); // solvent's mobility
+    solv_Props.w = atof(paras["w_s"].c_str()); // solvent's coefficients in phase field energy
+    solv_Props.a = atof(paras["a_s"].c_str());
+    solv_Props.b = atof(paras["b_s"].c_str());
+    solv_Props.Lambda = atof(paras["Lambda"].c_str()); // coupling strength
+    solv_Props.ro = radius; // outer radius
+    solv_Props.ri = radius/(double)(nR+1); // inner radius
+    solv_Props.nR = nR;  // space dimensions
+    solv_Props.nTheta = nTheta;
+    solv_Props.nPhi = nPhi;
     
-    InnerSolv inSolv(dt); // create the inner solvent
-    // initialization
-    double x0 = 1;
-    double y0 = 1;
-    double z0 = 1;
-    double radius = 1.5;
-    inSolv.initPsiSprYinGd(x0, y0, z0, radius, -1, 1);
+    
+    // create inner solvent
+    InnerSolv inSol(dt, solv_Props);
+    // initialize psi by a nucleus located at (x0, y0, z0) in Yin grid with radius=nu_rad
+    const double x0 = 1;
+    const double y0 = 1;
+    const double z0 = 1;
+    const double nu_rad = 3;
+    inSol.initPsiSprYinGd(x0, y0, z0, nu_rad, -1, 1);
     
     // evolve the dynamics
-    for (int i = 0; i <= Nstep; i++) {
-        inSolv.calcMu_sd();
+    for (int i = 0; i < nt; i++) {
+        // calculate chemical potential by mu=psi
+        inSol.calcMu_sd();
         
-        // store the state every NprintState steps
-		if ((i % NprintState) == 0) {
-            printMergeSolvState(inSolv, i, datafolder);
-        }
-
-        inSolv.updatePsi();
+        // store the state every nPrintState steps
+		if ((i % nPrintState) == 0) {
+			printMergeSolvState(inSol, i+t_start, datafolder);
+		}
+        
+        // update order parameters
+        inSol.updatePsi();
     }
     
     return 0;
 }
+
+
+
+
 
