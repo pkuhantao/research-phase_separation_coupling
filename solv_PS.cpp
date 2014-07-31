db17d004579dabbca219d630a53283a808dad64f
pkuhantao <pkuhantao@gmail.com>
2014-07-31 16:08:13 -0400

    print the inner solvent state by the format of Yin+center and Yang+center in their own spherical coordinates

--- solv_PS.cpp
+++ solv_PS.cpp
@@ -99,7 +99,7 @@ int main(int argc, char* argv[]) {
         
         // store the state every nPrintState steps
 		if ((i % nPrintState) == 0) {
-			printInSolvPatchesState_sfSph(inSol, i+t_start, datafolder);
+			printInSolvState_sfSph(inSol, i+t_start, datafolder);
 		}
         
         // store the analysis every nPrintAnaly steps
